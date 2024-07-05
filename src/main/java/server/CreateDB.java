08536990608eef4b0cbc858138d29aae54ac9835
Alexandre Queudeville <alexandre.queudeville@gmail.com>
2024-07-05 09:55:52 +0200

    changer le lien restant en localhost

--- src/main/java/server/CreateDB.java
+++ src/main/java/server/CreateDB.java
@@ -24,7 +24,7 @@ public class CreateDB {
      */
     public static void saveUserID(String userID) {
 
-        String url = "jdbc:mysql://10.34.6.84:3306/";
+        String url = "jdbc:mysql://localhost:3306/";
         String user = "root";
         String password = "password";
         
